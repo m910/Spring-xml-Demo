1f6494067a9be54b5b84bd345b3a1163f5b660ce
Megha <megha.kothale@cgi.com>
2019-07-26 17:14:43 +0530

    spring-xml firsttask is done

---
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -0,0 +1,34 @@
+package com.stackroute.domain;
+
+public class Actor {
+    private String actorName,gender;
+    private int age;
+
+
+    public String getActorName() {
+        return actorName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setActorName(String actorName) {
+        this.actorName = actorName;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,15 @@
+package com.stackroute.domain;
+
+public class Movie {
+    private Actor actorRef;
+
+    public Actor getActorRef() {
+        return actorRef;
+    }
+
+    public void setActorRef(Actor actorRef) {
+        this.actorRef = actorRef;
+    }
+
+    }
+
